31acd7368d60b89cae843beb04e99815766e8ecb
axal25 <emevig@gmail.com>
2023-07-09 14:03:43 +0200

    moved @EnabledAutoConfiguration(exclude=...) from separate @Configuration to Main Class @SpringBootApplication(exclude=...)

--- src/main/java/axal25/oles/jacek/CallRestApisApp.java
+++ src/main/java/axal25/oles/jacek/CallRestApisApp.java
@@ -2,8 +2,9 @@ package axal25.oles.jacek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class CallRestApisApp {
     public static void main(String[] args) {
         SpringApplication.run(CallRestApisApp.class, args);
